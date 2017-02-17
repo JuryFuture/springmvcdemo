2754d75f23b3cdd67d55a095015279e3efaf547b
滕国栋 <juryfuture@gmail.com>
2017-02-17 20:36:44 +0800

    声明注解

--- src/main/java/com/example/validation/annotation/Conditional.java
+++ src/main/java/com/example/validation/annotation/Conditional.java
@@ -1,9 +1,19 @@
 package com.example.validation.annotation;
 
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.CONSTRUCTOR;
+import static java.lang.annotation.ElementType.PARAMETER;
+import java.lang.annotation.Retention;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+import java.lang.annotation.Target;
+
 /**
  * @Description:
  * @Author tengguodong
  * @Date 2017/2/17 19:53
  */
+@Target({FIELD,METHOD,CONSTRUCTOR,PARAMETER})
+@Retention(RUNTIME)
 public @interface Conditional {
 }
--- src/main/java/com/example/validation/annotation/Validate.java
+++ src/main/java/com/example/validation/annotation/Validate.java
@@ -1,9 +1,17 @@
 package com.example.validation.annotation;
 
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.CONSTRUCTOR;
+import java.lang.annotation.Retention;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+import java.lang.annotation.Target;
 /**
  * @Description:
  * @Author tengguodong
  * @Date 2017/2/17 19:51
  */
+@Target({TYPE,METHOD,CONSTRUCTOR})
+@Retention(RUNTIME)
 public @interface Validate {
 }
